e6936d24e9d8eb9362f04f85ad710b45c8d68285
athreyashettyb <athreyashettyb@KTRAININD.COM>
2026-03-17 20:16:27 +0530

    Code updation

--- summarizer.py
+++ summarizer.py
@@ -3,13 +3,14 @@ from transformers import pipeline
 # Load summarization pipeline
 summarizer = pipeline("summarization")
 
+
 text = """
 Artificial intelligence (AI) is intelligence demonstrated by machines, 
 as opposed to natural intelligence displayed by animals including humans. 
 AI research has been defined as the field of study of intelligent agents.
 """
 
-# Generate summary
+
 summary = summarizer(text, max_length=50, min_length=20, do_sample=False)
 
 print(summary[0]['summary_text'])
